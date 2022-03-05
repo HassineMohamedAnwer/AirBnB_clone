c8bb2bdde64722a55594797663e0cd1cd54f9c19
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-03-05 09:50:41 -0800

    00000

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,21 @@ from datetime import datetime
 class BaseModel:
     """ first class   """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+        return "[{}] ({}) {}".format(__class__.__name__, self.id,
                                      self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        my_dict = dict(**self.__dict__)
-        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict = dict(self.__dict__)
+        my_dict['__class__'] = self.__class__.__name__
         my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
         return (my_dict)
