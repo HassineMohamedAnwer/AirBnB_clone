fbe4e347a8b713233b25a5d2da3588f528023e81
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-03-06 14:59:38 -0800

    l

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class city"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """inherits from basemodel"""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""nex class state """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """inherits frome basemodel"""
+    name = ""
