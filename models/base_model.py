bfe92838b86423db036a56e755880a5d7391894f
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-03-06 07:01:50 -0800

    savin

--- models/base_model.py
+++ models/base_model.py
@@ -11,16 +11,16 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
 
-    def __str__(self):
-        return "[{}] ({}) {}".format(__class__.__name__, self.id,
-                                     self.__dict__)
-
-    def save(self):
-        self.updated_at = datetime.now()
-
     def to_dict(self):
         my_dict = dict(self.__dict__)
         my_dict['__class__'] = self.__class__.__name__
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['created_at'] = self.created_at.isoformat()
         return (my_dict)
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(__class__.__name__, self.id,
+                                     self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
