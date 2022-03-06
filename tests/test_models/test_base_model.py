d3885cfdc31bf9a3b26c9a056413f6f2a5d94af3
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-03-06 14:30:50 -0800

    tests/

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" tests for base_model"""
+import models
+import unittest
+import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """ tests for basemodel"""
