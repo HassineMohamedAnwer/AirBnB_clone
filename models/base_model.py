f422758bca634c7a0d6fbf43a93883c57ad92e5c
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-03-06 14:22:42 -0800

    Authors, betty

--- models/base_model.py
+++ models/base_model.py
@@ -4,17 +4,20 @@ import models
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """ first class   """
 
     def __init__(self, *args, **kwargs):
+        tf = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    self.__dict__[key] = datetime.strptime(value,
+                                                           tf)
                 elif key == "__class__":
                     continue
                 self.__dict__[key] = value
