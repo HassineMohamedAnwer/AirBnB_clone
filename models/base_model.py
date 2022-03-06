8f15b43cea7fb89492e4063778f4669e8b9e20a0
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-03-06 07:19:21 -0800

    2

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,17 @@ class BaseModel:
     """ first class   """
 
     def __init__(self, *args, **kwargs):
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "__class__":
+                    continue
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def to_dict(self):
         my_dict = dict(self.__dict__)
